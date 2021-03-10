c953edaab8ee4194e42fdbceb93d29f11d4970d7
liviegladys <gladys.akela.pro@gmail.com>
2021-03-10 10:45:03 +0100

    update store

--- api/domain/domain_services/managers/storeManager.ts
+++ api/domain/domain_services/managers/storeManager.ts
@@ -7,6 +7,8 @@
 import storeModel from "../Models/storeModel";
 import { DbConnection } from "../../Data/dbConnection";
 import { CryptPassword } from "../schema_services/cryptPassword";
+import * as bcrypt from "bcrypt";
+import { match } from "node:assert";
 
 export class StoreManager {
 
@@ -53,7 +55,40 @@ export class StoreManager {
                     })
                 }
               }
+   
+             public async updateStore(req,res){
+                if(req.body.Email != undefined && req.body.Email !=null ){
+                    const store= await storeModel.findOne({Email:req.body.Email})
+                    if(store == null){
+                        res.status(400).send({message:"le magasin n'existe pas"})
+                        }else if(req.body.NewEmail != undefined && req.body.NewEmail != null && req.body.NewEmail!=""){
+                            await bcrypt.compare(req.body.Password,store.Password,(err,match)=>{
+                                if (err) {
+                                    res.status(400).send({message:"le mot de passe ne correspond pas au mot de passe de l email du magasin"})
+                                } else  if (match==true){
+      
+                                    store.updateOne({ Email: (req.body.NewMail )},{new:true},(err,newStore) => {
+                                        if (err) {
+                                            res.status(400).send({message:"le nouveau email est incorrect"})
+                                        }else{
+                                            res.status(200).send({message:"la mise à jour a bien été effectuée"})
+                                        }
+                                    })
+                                }else{
+                                    res.status(400).send({message:"la mise à jour ne peut etre effectué car les mots de passe ne correspondent pas"})
+                                }
+                            })
+                        }
+                }
                 
+             }
 
 
 }
+
+
+
+
+
+
+
