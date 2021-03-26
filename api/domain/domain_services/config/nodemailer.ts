ce90b8a8db75cd32d2789079ac20c5790230f048
liviegladys <gladys.akela.pro@gmail.com>
2021-03-26 11:52:24 +0100

    nodemail1

--- api/domain/domain_services/config/nodemailer.ts
+++ api/domain/domain_services/config/nodemailer.ts
@@ -6,18 +6,23 @@ import * as dotenvExpand  from "dotenv-expand";
 
 export class SendMail {
 
-      private _transporter: nodemailer.Transporter; 
+  private _transporter: nodemailer.Transporter; 
       constructor() { 
-        this._transporter = nodemailer.createTransport( 
-          `smtps://gladys%40gmail.com:gladys@smtp.gmail.com` 
-        ); 
+        this._transporter = nodemailer.createTransport({
+         service:"gmail",
+          auth:{
+              user:process.env.Email,
+              password:process.env.password
+          }
+        }  ); 
+         
       } 
       sendMail(to: string, subject: string, content: string) { 
         let options = { 
           from: 'gladys.akela.pro@gmail.com', 
-          to: to, 
-          subject: subject, 
-          text: content 
+          to: "gladysakela2016@gmail.com", 
+          subject: "test", 
+          text: "Apprentissage" 
         } 
  
         this._transporter.sendMail(  
